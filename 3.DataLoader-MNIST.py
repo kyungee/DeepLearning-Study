410125aa89553c6ae27be99769e4dbfd6e359f41
kyungee <kyungee.kim@gmail.com>
2019-04-03 21:47:52 +0900

    2주차: 코드 설명 주석 추가

--- 3.DataLoader-MNIST.py
+++ 3.DataLoader-MNIST.py
@@ -10,10 +10,13 @@ from torch.autograd import Variable
 
 mnistdata = datasets.MNIST('./data/', train=True, download=True, transform=transforms.ToTensor())
 print('number of image : ',len(mnistdata))
+# mnistdata[10][0]: x값, 이미지 정보
+# mnistdata[10][1]: y값, label
 
 batch_size = 10
 print('Data Loader')
 data_loader = torch.utils.data.DataLoader(dataset=mnistdata, batch_size=batch_size, shuffle=True)
+# SGD mini-batch 생성
 
 count = 0
 for batch_idx, (data, target) in enumerate(data_loader):
