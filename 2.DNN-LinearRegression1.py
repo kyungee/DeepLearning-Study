9ad77d9dfe25458e5a89eec753ce04057f278eb0
kyungee <kyungee.kim@gmail.com>
2019-04-03 21:17:35 +0900

    코드 설명 주석 추가

--- 2.DNN-LinearRegression1.py
+++ 2.DNN-LinearRegression1.py
@@ -29,18 +29,30 @@ y_data = torch.LongTensor([
 class DNN(nn.Module):
     def __init__(self):
         super(DNN, self).__init__()
-        self.w = nn.Linear(2, 3)
+        self.w = nn.Linear(2, 3) ## 2개 input을 받아 3 labels classification
         self.bias = torch.zeros([3])
+        
+       ## Layer를 쌓아보자! 
+        ## self.w = nn.Linear(2, 10)
+        ## self.bias = torch.zeros([10])
+        ## self.w2 = nn.Linear(10, 3) 
+        ## self.bias2 = torch.zeros([3])
         self.relu = nn.ReLU()
 
     def forward(self, x):
         y = self.w(x) + self.bias
         y = self.relu(y)
+        
+       ## Layer를 쌓아보자! 
+        ## y = self.w(x) + self.bias
+        ## y = self.relu(y)
+        ## y = self.w2(y) + self.bias2
+        ## y = self.relu(y)
         return y
 
 model = DNN()
 
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.CrossEntropyLoss() ## multi classification에서 loss(cost) function으로 쓰는거!
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 for epoch in range(1000):
@@ -50,9 +62,9 @@ for epoch in range(1000):
     loss = criterion(output, y_data)
 
     # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
+    optimizer.zero_grad() ## 역전파시 영향을 미치는 가중치의 미분 값을 초기화
     loss.backward()
-    optimizer.step()
+    optimizer.step() ## 계속 업데이트 해줌
 
     print("progress:", epoch, "loss=", loss.item())
 
